427922619e6f8449a789215bd473ac4807f546df
Omer <27678413+TheOmer77@users.noreply.github.com>
2022-08-20 22:13:30 +0300

    Add graphics layer to map in App, using newly added components
    There seem to be some TS errors, ArcGIS typedefs are incorrect I guess

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Graphic from './components/Graphic';
+import GraphicsLayer from './components/layers/GraphicsLayer';
 import MapView from './components/MapView';
 
 const App = () => {
@@ -6,7 +8,63 @@ const App = () => {
       <MapView
         mapProps={{ basemap: 'arcgis-topographic' }}
         mapViewProps={{ center: [-118.805, 34.027], zoom: 13 }}
-      />
+      >
+        <GraphicsLayer>
+          <Graphic
+            key='testPoint'
+            geometry={{
+              type: 'point',
+              longitude: -118.80657463861,
+              latitude: 34.0005930608889,
+            }}
+            symbol={{
+              type: 'simple-marker',
+              color: [226, 119, 40], // Orange
+              outline: {
+                color: [255, 255, 255], // White
+                width: 1,
+              },
+            }}
+          />
+          <Graphic
+            key='testLine'
+            geometry={{
+              type: 'polyline',
+              paths: [
+                [-118.821527826096, 34.0139576938577], //Longitude, latitude
+                [-118.814893761649, 34.0080602407843], //Longitude, latitude
+                [-118.808878330345, 34.0016642996246], //Longitude, latitude
+              ],
+            }}
+            symbol={{
+              type: 'simple-line',
+              color: [226, 119, 40], // Orange
+              width: 2,
+            }}
+          />
+          <Graphic
+            key='testPolygon'
+            geometry={{
+              type: 'polygon',
+              rings: [
+                [-118.818984489994, 34.0137559967283], //Longitude, latitude
+                [-118.806796597377, 34.0215816298725], //Longitude, latitude
+                [-118.791432890735, 34.0163883241613], //Longitude, latitude
+                [-118.79596686535, 34.008564864635], //Longitude, latitude
+                [-118.808558110679, 34.0035027131376], //Longitude, latitude
+              ],
+            }}
+            symbol={{
+              type: 'simple-fill',
+              color: [227, 139, 79, 0.8], // Orange, opacity 80%
+              outline: {
+                color: [255, 255, 255],
+                width: 1,
+              },
+            }}
+          />
+        </GraphicsLayer>
+      </MapView>
     </>
   );
 };
